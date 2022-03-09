7e2e50d35c821d9db094e45e996034629bd21195
marcosalexandrebj <marcosalexandrebj@gmail.com>
2022-03-09 15:09:28 -0500

    New-Feature

--- main.py
+++ main.py
@@ -4,13 +4,18 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi_1(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi MABJ, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def print_hi_2():
+    # Use a breakpoint in the code line below to debug your script.
+    print('Testing')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm-Feature-Review')
+    print_hi_1('PyCharm-Feature-Review')
+    print_hi_2()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
